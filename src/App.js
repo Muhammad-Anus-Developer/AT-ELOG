50b7542281fb9a9a6809b9db6b530857b21349f2
Muhammad Anus <80568552+Muhammad-Anus-Developer@users.noreply.github.com>
2021-11-04 17:20:01 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-// import Navbar from './components/navbar';
+
 import SideBar1 from './components/sidebar1';
-import SideBar from './components/sidebar1';
+
+// import Navbar from './components/navbar';
+// import SideBar1 from './components/sidebar1';
+
 
 function App() {
   return (
     <div>
-    <SideBar />
+    <SideBar1 />
     </div>
   );
 }
